91d7c2c6a488d68b4a4faed06f1395df50490127
Justin Oakes <admin@oklasoftware.com>
2017-12-20 21:25:27 -0600

    persists selected theme

--- Marco Polo/Views/SettingsViewController.swift
+++ Marco Polo/Views/SettingsViewController.swift
@@ -17,6 +17,28 @@ class SettingsViewController: UIViewController {
         super.viewDidLoad()
         themePickerTextfield.picker.delegate = self
         themePickerTextfield.picker.dataSource = self
+        switch theme() {
+        case .standard:
+            themePickerTextfield.picker.selectRow(0, inComponent: 0, animated: false)
+            themePickerTextfield.text = NSLocalizedString("Default", comment: "Default")
+            break
+        case .spaceGrey:
+            themePickerTextfield.picker.selectRow(1, inComponent: 0, animated: false)
+            themePickerTextfield.text = NSLocalizedString("Space Gray", comment: "Space Gray")
+            break
+        case .silver:
+            themePickerTextfield.picker.selectRow(2, inComponent: 0, animated: false)
+            themePickerTextfield.text = NSLocalizedString("Silver", comment: "Silver")
+            break
+        case .gold:
+            themePickerTextfield.picker.selectRow(3, inComponent: 0, animated: false)
+            themePickerTextfield.text = NSLocalizedString("Gold", comment: "Gold")
+            break
+        case .roseGold:
+            themePickerTextfield.picker.selectRow(4, inComponent: 0, animated: false)
+            themePickerTextfield.text = NSLocalizedString("Rose Gold", comment: "Rose Gold")
+            break
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -37,7 +59,14 @@ class SettingsViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    
+    func theme() -> themes {
+        guard let theme: themes = themes(rawValue: UserDefaults.standard.string(forKey: "theme") ?? "")
+            else {
+                return .standard
+        }
+        return theme
+    }
 }
 
 extension SettingsViewController: UIPickerViewDataSource {
@@ -74,16 +103,38 @@ extension SettingsViewController: UIPickerViewDelegate {
         switch row {
         case 0:
             themePickerTextfield.text = NSLocalizedString("Default", comment: "Default")
+            UserDefaults.standard.set(themes.standard.rawValue, forKey: "theme")
+            break
         case 1:
             themePickerTextfield.text = NSLocalizedString("Space Gray", comment: "Space Gray")
+            UserDefaults.standard.set(themes.spaceGrey.rawValue, forKey: "theme")
+            break
         case 2:
             themePickerTextfield.text = NSLocalizedString("Silver", comment: "Sliver")
+            UserDefaults.standard.set(themes.silver.rawValue, forKey: "theme")
+            break
         case 3:
             themePickerTextfield.text = NSLocalizedString("Gold", comment: "Gold")
+            UserDefaults.standard.set(themes.gold.rawValue, forKey: "theme")
+            break
         case 4:
             themePickerTextfield.text = NSLocalizedString("Rose Gold", comment: "Rose Gold")
+            UserDefaults.standard.set(themes.roseGold.rawValue, forKey: "theme")
+            break
         default:
             themePickerTextfield.text = NSLocalizedString("Default", comment: "Default")
+            UserDefaults.standard.set(themes.standard.rawValue, forKey: "theme")
         }
+        
     }
+}
+
+enum themes: String {
+    typealias RawValue = String
+    
+    case standard = ""
+    case spaceGrey = "spaceGrey"
+    case silver = "silver"
+    case gold = "gold"
+    case roseGold = "roseGold"
 }
